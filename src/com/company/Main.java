8fd318e7e1d9c1d49c88df593a65bc7ebc4953d2
Adrian <adrian131313@wp.pl>
2021-03-27 11:59:12 +0100

    Add lines Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
+        System.out.println("Pisze pisze pisze");
     }
 }
 
