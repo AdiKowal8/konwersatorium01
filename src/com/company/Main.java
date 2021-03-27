0aa5c3768f44c58e5f68c4d5eecf2a346fbf612f
Adrian <adrian131313@wp.pl>
2021-03-27 12:07:05 +0100

    Add class Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Piszefpisze pisze");
         System.out.println("Pissdfsdpisze pisze");
-        System.out.println("Piszfsdfsisze pisze");
-        System.out.println("Pisze fsdfisze pisze");
         System.out.println("Pisze dfsdfe pisze");
         System.out.println("Pisze pisdfsdfe pisze");
         System.out.println("Psafsdf");
